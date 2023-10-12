40da40d653cae3ea703e0d2cb32e737b06380d9c
esdb1 <pierre.lacognata@mail-esd.com>
2023-10-12 10:50:14 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,61 @@
+// alert('coucou')
+
+// bonjour je suis une feuille de javaScript
+//ceci est un commentaire
+//scope
+
+let mot = "carotte"
+
+//j'initialise (let) la variable "nombre" et je lui
+//assigne la valeur 12
+let nombre = 12
+
+
+
+console.log("coucou")
+
+console.log("----------------------")
+console.log("pasteque")
+console.log("----------------------")
+
+console.log(mot)
+console.log("----------------------")
+console.log(nombre)
+
+nombre = 14
+console.log("----------------------")
+console.log(nombre)
+
+mot = "camembert"
+console.log(mot)
+
+let plusGrandNombre = nombre + nombre +3 - 6
+
+console.log(plusGrandNombre)
+
+let nombreBizarre = nombre + mot
+
+
+console.log(nombreBizarre)
+
+let nombreChelou = "135"
+
+// concatenation
+
+console.log(mot - nombre)
+
+console.log(nombre + nombre )
+
+let encoreUnNombre = nombre + nombre
+
+let word = "aspirine"
+
+
+word = "coulommiers"
+
+let nouveauMot = word[5]+word[7]+word[5]+word[7]
+
+console.log(nouveauMot)
+
+
+
