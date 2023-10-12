63cf33fab15b2be64c9dfb2ede72cc378dc98fb4
esdb1 <pierre.lacognata@mail-esd.com>
2023-10-12 14:53:42 +0200

    added things

--- main.js
+++ main.js
@@ -1,61 +1,105 @@
-// alert('coucou')
 
-// bonjour je suis une feuille de javaScript
-//ceci est un commentaire
-//scope
+let word = "bonjour"
 
-let mot = "carotte"
+let prenom = "jean-michel"
 
-//j'initialise (let) la variable "nombre" et je lui
-//assigne la valeur 12
+//  dans la console : bonjour jean-michel
+
+let phrase = word + " " + prenom
+
+console.log(phrase)
+
+
+let longueur = phrase.length
+
+
+// dans la console : bonjour jean-patrick
+// remplacer michel par patrick
+console.log(longueur)
+let nouvellePhrase =  phrase.replace("michel","patrick")
+
+console.log(phrase)
+console.log(nouvellePhrase)
+
+///   que l'on obtienne "bonjour jean
+//  de deux manieres différentes : avec replace() et avec
+//  une nouvelle methode
+
+let phraseCoupee = nouvellePhrase.replace("-patrick","")
+
+console.log(phraseCoupee)
+
+nouvellePhrase = "bonjour jean-patrick"
+
+phraseCoupee = nouvellePhrase.slice(0,12)
+
+console.log(phraseCoupee)
+
+let mot = "coucou"
 let nombre = 12
+let isYellow = true
+let isGreen = false
+
+let tableau = [
+                "coucou",
+                "pasteque",
+                12,
+                "carotte",
+                 [
+                     "chat",
+                     "chien",
+                     "canne à pêche"
+                 ]
+              ]
+console.log(tableau[4][2])
+
+let maPhrase = "le doux dos dodu de ce dindon bleu danse et se dandine"
+
+let mots = maPhrase.split(" ")
+
+console.log(mots)
+// retirer l'élement "bleu" du tableau
+mots.splice(7,1)
+
+console.log("mots : ")
+console.log(mots)
+
+let phraseCorrigee = mots.join(" ")
+
+console.log(phraseCorrigee)
+//reconstruire une chaine de caracteres let phraseCorrigee
 
 
 
-console.log("coucou")
 
-console.log("----------------------")
-console.log("pasteque")
-console.log("----------------------")
-
-console.log(mot)
-console.log("----------------------")
-console.log(nombre)
-
-nombre = 14
-console.log("----------------------")
-console.log(nombre)
-
-mot = "camembert"
-console.log(mot)
-
-let plusGrandNombre = nombre + nombre +3 - 6
-
-console.log(plusGrandNombre)
-
-let nombreBizarre = nombre + mot
+/// accéder à un élément du DOM
+const maBoiteAChoucroute = document.querySelector(".boiteChoucroute")
 
 
-console.log(nombreBizarre)
-
-let nombreChelou = "135"
-
-// concatenation
-
-console.log(mot - nombre)
-
-console.log(nombre + nombre )
-
-let encoreUnNombre = nombre + nombre
-
-let word = "aspirine"
+const  monChampTexte =document.querySelector(".message")
+const monBouton = document.querySelector(".monBouton")
 
 
-word = "coulommiers"
+monBouton.addEventListener("click", ()=>{
+    //trouve le moyen de vérifier si l'utilisateur n'a pas utilisé le mot quiche
+    // si c'est le cas, on n'accepter pas sa phrase, et on la remplace par
+    // désolé "quiche" n'est pas un mot autorisé
 
-let nouveauMot = word[5]+word[7]+word[5]+word[7]
+    maBoiteAChoucroute.innerHTML += ("<p>"+monChampTexte.value+"</p>")
+    monChampTexte.value = ""
+} )
 
-console.log(nouveauMot)
+let unPeu = "pas beaucoup"
+let beaucoup = "pas peu"
+
+beaucoup += unPeu
+
+console.log(beaucoup)
+
+
+
+
+
 
 
 
